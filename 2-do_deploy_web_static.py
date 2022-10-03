7ce3e57eb534de0234bd4da0a3aba8d7a4720308
Esther <99372812+EstherLard@users.noreply.github.com>
2022-10-02 22:34:05 -0700

    deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,7 +9,7 @@ from datetime import datetime
 from fabric.api import env, local, put, run, runs_once
 
 
-env.hosts = ['34.138.32.248', '3.226.74.205']
+env.hosts = ['44.192.25.140', '44.200.66.235']
 
 
 def do_deploy(archive_path):
